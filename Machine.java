3232b279ec141e0fdf3df0a06609a2bef49f6075
babqs <55034150+babqs@users.noreply.github.com>
2019-09-10 12:41:14 -0300

    Add files via upload

--- Machine.java
+++ Machine.java
@@ -1,12 +1,15 @@
 
-package machine;
+package turingmachine1;
+
+//import java.util.ArrayList;
 
 import java.util.Scanner;
 
-public class Machine {
 
+public class Machine {
+    
     char fita[];
-  int contador;
+    int contador;
     
     public void Q(String palavra1){
         contador = 0;
@@ -15,181 +18,157 @@ public class Machine {
     }
     
     public void q0(){
-        if(contador <= 0 ){
-            if(fita[contador] == 'Z' && fita[contador + 1] == 'Z'){
-                //fita[contador] = 'Z';
-                contador++;
+        if(fita.length > contador){
+            if(fita[contador] == 'a'){
+                fita[contador] ='A';
                 System.out.println(fita);
-                q5f();
-            }
-            else if(fita[contador + 1] == 'a'){
-                contador++;
-                fita[contador] = 'A';
-                System.out.println(fita);
-                contador++;
+                contador = contador + 1;
                 q1();
             }
-            else{
+            else if(fita.length == 'B'){
+                fita[contador] = 'B';
                 System.out.println(fita);
-                qErro();
+                contador = contador + 1;
+                q4();
+            }
+            else{
+                q5f();
             }
         }
-        else if(contador >= 0){
-            if(fita[contador] == 'a'){
-                fita[contador] = 'A';
-                System.out.println(fita);
-                contador++;
-            }           q1();
-    }
-//        else{
-//            System.out.println(fita);
-//            qErro();
-//        }
-}
-    public void q1(){
-        if(contador >= 0){
-            if(fita[contador] == 'a'){
-            fita[contador] = 'a';
-            System.out.println(fita);
-            contador++;
-            q1();
-        }
-            else if(fita[contador] == 'B'){
-            fita[contador] = 'B';
-            System.out.println(fita);
-            contador++;
-            q1();
-        }
-            else if(fita[contador] == 'b'){
-            fita[contador] = 'B';
-            System.out.println(fita);
-            contador++;
-            q2();
-        }
-            else{
-                System.out.println(fita);
+           else{
+                System.out.println("Erro no q0");
                 qErro();
+            }
+    }
+    public void q1(){
+        if(fita.length > contador){
+            if(fita[contador] == 'a'){
+                fita[contador] = 'a';
+                System.out.println(fita);
+                contador = contador + 1;
+                q1();
+            }
+            else if(fita[contador] == 'B'){
+                fita[contador] = 'B';
+                System.out.println(fita);
+                contador = contador + 1;
+                q1();
+            }
+            else if(fita[contador] == 'b'){
+                fita[contador] = 'B';
+                System.out.println(fita);
+                contador = contador + 1;
+                q2();
+            }
+            else{
+                contador = contador + 1;
+                q1();
         }
     }
-        
-//        else if(fita[contador] == 'b'){
-//            fita[contador] = 'B';
-//            System.out.println(fita);
-//            contador++;
-//            q2();
-//        }
-//        else{
-//            System.out.println(fita);
-//            qErro();
-//        }
+        else{
+            System.out.println("Erro no q1");
+            qErro();
+        }
     }
     public void q2(){
-        if(contador >= 0){
+        if(fita.length > contador){
         if(fita[contador] == 'b'){
             fita[contador] = 'b';
             System.out.println(fita);
-            contador++;
+            contador = contador + 1;
             q2();
         }
         else if(fita[contador] == 'C'){
             fita[contador] = 'C';
             System.out.println(fita);
-            contador++;
+            contador = contador + 1;
             q2();
         }
         else if(fita[contador] == 'c'){
             fita[contador] = 'C';
             System.out.println(fita);
-            contador--;
+            contador = contador - 1;
             q3();
         }
         else{
-            System.out.println(fita);
+            contador = contador + 1;
+            q2();
+        }
+        }
+        else{
+            System.out.println("Erro no q2");
             qErro();
         }
     }
-//        else{
-//            System.out.println(fita);
-//            qErro();
-//        }
-    }
+    
     public void q3(){
-        if(contador >= 0){
+        if(fita.length > contador){
         if(fita[contador] == 'b'){
             fita[contador] = 'b';
             System.out.println(fita);
-            contador--;
+            contador = contador - 1;
             q3();
         }
         else if(fita[contador] == 'B'){
             fita[contador] = 'B';
             System.out.println(fita);
-            contador--;
+            contador = contador - 1;
             q3();
         }
         else if(fita[contador]=='a'){
             fita[contador] = 'a';
             System.out.println(fita);
-            contador--;
+            contador = contador - 1;
             q3();
         }
         else if(fita[contador] == 'C'){
             fita[contador] = 'C';
             System.out.println(fita);
-            contador--;
+            contador = contador - 1;
             q3();
         }
         else if(fita[contador] == 'A'){
             fita[contador] = 'A';
             System.out.println(fita);
-            contador++;
+            contador = contador + 1;
             q0();
         }
         else{
-            System.out.println(fita);
+            System.out.println("Erro no q3");
             qErro();
         }
     }
-//        else if(fita[contador] == 'C'){
-//            fita[contador] = 'C';
-//            System.out.println(fita);
-//            contador++;
-//            q4();
-//        }
-//        else{
-//            System.out.println(fita);
-//            qErro();
-//        }
-    }
+}
     public void q4(){
-        if(contador >= 0){
-            if(fita[contador] == 'Z' && fita[contador - 1] == 'C'){
-            //fita[contador] = 'Z';
-            System.out.println(fita);
-            contador++;
-            q5f();
-        }
-            else if(fita[contador] == 'C'){
-            fita[contador] = 'C';
-            System.out.println(fita);
+        if(fita.length > contador){
+        if(fita[contador] == 'B'){
+            fita[contador] = 'B';
             contador++;
             q4();
         }
-        else{
+        else if(fita[contador] == 'C'){
+            fita[contador] = 'C';
             System.out.println(fita);
+            contador = contador + 1;
+            q4();
+        }
+        else if(fita[contador] == fita.length -1){
+            //fita[contador] = ' ';
+            contador++;
+            q5f();
+        }
+        else{
+            System.out.println("Erro no q4");
             qErro();
         }
     }
         else{
-            System.out.println(fita);
             q5f();
         }
     }
-    
     public void q5f(){
         System.out.println("Palavra aceita!");
     }
-    
     public void qErro(){
         System.out.println("Palavra nao aceita!");
     }
@@ -199,8 +178,7 @@ public class Machine {
         Machine m = new Machine();
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("De um espaco antes de iniciar a palavra e apos terminar a palavra.");
-        System.out.println("Insira a palavra: ");
+        System.out.println("Insira a palavra:");
         String palavra = scan.nextLine();
         
         m.Q(palavra);
